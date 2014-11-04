8047101f2e477b0e9230fbc4b42374f598034843
YanfengZhao <yanfeng.zhao@utexas.edu>
2014-11-03 20:28:13 -0600

    change coverage

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -2,11 +2,6 @@ import unittest
 import Experiment
 
 class MyTestCase(unittest.TestCase):
-
-  def test_t1(self):
-    r1 = Experiment.largest([1,1,1]) 
-    self.assertEqual(r1, 1)
-
   def test_t2(self):
     r1 = Experiment.largest([3,2,1]) 
     self.assertEqual(r1, 3)
