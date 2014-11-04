b6377f7e21c4ef4d5a1cfc18ace0c52155443170
YanfengZhao <yanfeng.zhao@utexas.edu>
2014-11-03 20:32:40 -0600

    final submit

--- nose.sh
+++ nose.sh
@@ -1,1 +1,2 @@
-nosetests -v --with-coverage ExperimentTest.py
+nosetests -v --with-xunit --all-modules --traverse-namespace --with-coverage --cover-inclusive ExperimentTest.py 2> output.txt
+python -m coverage xml --include=*
